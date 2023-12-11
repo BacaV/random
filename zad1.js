4a3f0d79cba0735ed1ce8171732d42eb0bc4c13a
BacaV <nikolajevtic3405@gmail.com>
2023-12-11 16:32:06 +0100

    Drugi zadatak

--- zad1.js
+++ zad1.js
@@ -3,3 +3,15 @@ function plusPlus(orig_x) {
     x = orig_x_coerced +1;
     return x;
 }
+
+function izracunajFaktorijel(num) {
+    var result = num;
+    if (num === 0 || num === 1) 
+    return 1; 
+    while (num > 1) { 
+    num--;
+    result *= num;
+    }
+    return result;
+}
+console.log(izracunajFaktorijel(5));
